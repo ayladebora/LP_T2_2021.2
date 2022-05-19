57e0b10294fff49288794c65c83206b8972bb8d3
Ayla Dantas Rebouças <ayla@dce.ufpb.br>
2022-05-19 20:19:49 -0300

    Códigos da aula do dia 19 de maio de 2022

---
+++ aula19Maio/src/main/java/br/ufpb/ayla/banco/MenuBanco.java
@@ -0,0 +1,10 @@
+package br.ufpb.ayla.banco;
+
+import java.util.ArrayList;
+
+public class MenuBanco {
+
+    public static void main(String [] args){
+        BancoList banco = new BancoList("MyBank", "555/555-87", new ArrayList<>());
+    }
+}
